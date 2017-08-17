53b970058b03cf843a1594c21f869078c643593f
Lucien Brillant <briluc21@evergreen.edu>
2017-08-17 15:14:59 -0700

    prototype/first comiit

---
+++ knock.go
@@ -0,0 +1,125 @@
+// Go implementation of port knocking.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Knocker interface {
+	Knock(...Knock)
+}
+
+// A single knock to either listen for or execute.
+type Knock struct {
+	// either udp or tcp
+	Type string
+	// listen on
+	Port string
+	// bytes to send
+	PayLoad []byte
+}
+
+type Listener struct {
+	Host string
+}
+
+func Port(number int) Knock {
+	return Knock{
+		Type:    "tcp",
+		Port:    ":" + strconv.Itoa(number),
+		PayLoad: []byte{},
+	}
+}
+
+func Listen(knocks ...Knock) (successes chan string, err error) {
+	attempts := make(map[string]int)
+	lock := sync.RWMutex{}
+	var end int = len(knocks)
+	successes = make(chan string, 5)
+	for i, knock := range knocks {
+		var ln net.Listener
+		step := i + 1
+		ln, err = net.Listen(knock.Type, knock.Port)
+		if err != nil {
+			return
+		}
+		go func() {
+			for {
+				conn, err := ln.Accept()
+				if err != nil {
+					continue
+				}
+				go func() {
+					defer conn.Close()
+					lock.RLock()
+					source := strings.Split(conn.RemoteAddr().String(), ":")[0]
+					stage := attempts[source]
+					lock.RUnlock()
+					lock.Lock()
+					switch {
+					case stage == step-1:
+						recv, err := ioutil.ReadAll(conn)
+						if err != nil {
+							// TODO: something ?
+						}
+						if byteEqual(recv, knocks[step-1].PayLoad) {
+							attempts[source], stage = step, step
+							break
+						}
+						fallthrough
+					default:
+						delete(attempts, source)
+					}
+					lock.Unlock()
+					if stage == end {
+						successes <- source
+					}
+					log.Print(source)
+				}()
+			}
+		}()
+	}
+	return
+}
+
+func Send(host string, knocks ...Knock) {
+	for _, knock := range knocks {
+	retry:
+		conn, err := net.Dial(knock.Type, host+knock.Port)
+		if err != nil {
+			conn.Close()
+			goto retry
+		}
+		conn.Write(knock.PayLoad)
+		conn.Close()
+	}
+}
+
+func main() {
+	kocks := []Knock{
+		Knock{
+			Type:    "tcp",
+			Port:    ":8080",
+			PayLoad: []byte(":)"),
+		},
+		Port(8081),
+		Port(8082),
+		Port(8083),
+		Port(8084),
+	}
+	s, err := Listen(kocks...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go Send("localhost", kocks...)
+	select {
+	case ip := <-s:
+		fmt.Printf("WOOOOOO!! %s has Knocked\n", ip)
+	}
+}
---
+++ util.go
@@ -0,0 +1,15 @@
+package main
+
+func byteEqual(slice1, slice2 []byte) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	for i := range slice1 {
+		if slice1[i] != slice2[i] {
+			return false
+		}
+	}
+
+	return true
+}
